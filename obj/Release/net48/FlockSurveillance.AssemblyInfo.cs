6f57fa529c710af8a9d510dcfecf1bccfe9bea0b
WTTDOTM <wttdotm@gmail.com>
2026-08-30 23:23:30 -0700

    IN BETA BABYYYYY

--- obj/Release/net48/FlockSurveillance.AssemblyInfo.cs
+++ obj/Release/net48/FlockSurveillance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GTALPR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3762b648c2b4f7d6900ebd96e3358b2208f53dd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b7af57955e65b78f62029dc68022fb578cf6a27")]
 [assembly: System.Reflection.AssemblyProductAttribute("GTALPR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GTALPR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
