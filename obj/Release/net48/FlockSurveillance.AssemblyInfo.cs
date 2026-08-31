3beecca28ef1aa1b71ee152c38e02e8c15847811
WTTDOTM <wttdotm@gmail.com>
2026-08-31 11:36:24 -0700

    update instructions and package

--- obj/Release/net48/FlockSurveillance.AssemblyInfo.cs
+++ obj/Release/net48/FlockSurveillance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GTALPR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b7af57955e65b78f62029dc68022fb578cf6a27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481ef3201dca8d7470a971e51c35cd2b98b70fbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("GTALPR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GTALPR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
